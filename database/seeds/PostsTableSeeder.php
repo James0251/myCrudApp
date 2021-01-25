42c57ae873c3b5150ef4ba8880a616b610d44ab4
James0251 <den4643@yandex.ru>
2021-01-25 07:45:20 +0300

    Создал модель+миграцию+контроллер, создал seeder, заполнил информацию с помощью Faker, отправил данные в БД

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('ru_RU');
+        foreach (range(1, 5) as $index){
+            DB::table('posts')->insert([
+                'name' => $faker->jobTitle,
+                'detail' => $faker->text,
+                'author' => $faker->name,
+                'created_at' => $faker->dateTime($max = 'now'),
+                'updated_at' => $faker->dateTime($max = 'now'),
+            ]);
+        }
+    }
+}
